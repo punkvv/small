e815b32b2bb4935bf7059170abbe61afb9f2274c
PunkVv <740087240@qq.com>
2017-12-22 11:05:58 +0800

    2017122211

---
+++ small-api/application/admin/controller/SessionController.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Author: PunkVv <punkv@qq.com>
+ */
+
+namespace app\admin\controller;
+
+class SessionController extends Controller
+{
+    public function save()
+    {
+
+    }
+
+    public function read()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
--- small-api/route/route.php
+++ small-api/route/route.php
@@ -1,19 +1,17 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
+
+/**
+ * API 路由
+ * Author: PunkVv <punkv@qq.com>
+ */
 
 use think\facade\Route;
 
 // 后台路由
 Route::group('admin', function () {
-
+    Route::post('sessions', 'session/save'); // 登录
+    Route::get('sessions/:token', 'session/read'); // 获取登录用户信息
+    Route::delete('sessions/:token', 'session/delete'); // 注销
 });
 
 Route::miss('index/miss');
