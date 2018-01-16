c2b0e284f868ad72e32d5d2f63e1ff2a0b7a5706
PunkVv <740087240@qq.com>
2018-01-16 18:04:49 +0800

    2018011618

--- small-admin/src/libs/errorLog.js
+++ small-admin/src/libs/errorLog.js
@@ -3,21 +3,21 @@ import store from '../store'
 import {addErrorLog} from '@/api/base'
 
 // you can set only in production env show the error-log
-// if (process.env.NODE_ENV === 'production') {
-Vue.config.errorHandler = (err, vm, info, a) => {
-  // Don't ask me why I use Vue.nextTick, it just a hack.
-  // detail see https://forum.vuejs.org/t/dispatch-in-vue-config-errorhandler-has-some-problem/23500
-  const url = window.location.href
-  Vue.nextTick(() => {
-    store.dispatch('addErrorLog', {
-      err,
-      vm,
-      info,
-      url: url
+if (process.env.NODE_ENV === 'production') {
+  Vue.config.errorHandler = (err, vm, info, a) => {
+    // Don't ask me why I use Vue.nextTick, it just a hack.
+    // detail see https://forum.vuejs.org/t/dispatch-in-vue-config-errorhandler-has-some-problem/23500
+    const url = window.location.href
+    Vue.nextTick(() => {
+      store.dispatch('addErrorLog', {
+        err,
+        vm,
+        info,
+        url: url
+      })
+      // 提交到服务器
+      addErrorLog(url, err.message + ':' + vm.$vnode.tag)
+      console.error(err, info)
     })
-    // 提交到服务器
-    addErrorLog(url, err.message + ':' + vm.$vnode.tag)
-    console.error(err, info)
-  })
+  }
 }
-// }
